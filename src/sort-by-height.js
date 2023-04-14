31b74550ab14f273a0708016c823cfb1a887bc92
Kate-Shepel <kateborichevskaya@gmail.com>
2023-04-14 18:58:49 +0200

    feat: add sort-by-height comments to the solution

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -12,21 +12,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
 
-arr = [-1, 150, 190, 170, -1, -1, 160, 180]
-
 function sortByHeight(arr) {
   const indexArray = [];
+  // get sorted array without -1
   const sortedArray = arr.filter((num) => num !== -1).sort((a, b) => a - b);
-  console.log(sortedArray);
 
+  //get indexes where -1 was located
   arr.forEach((num, index) => {
     if (num === -1) {
       indexArray.push(index);
     }
   });
 
-  console.log(indexArray);
-
+  //to insert -1 to the place where is was located previously
   indexArray.forEach((index) => {
     sortedArray.splice(index, 0, -1);
   });
