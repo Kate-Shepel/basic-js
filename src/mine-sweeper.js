0b8c3d6406069caed56e96d283bf378734263450
Kate-Shepel <kateborichevskaya@gmail.com>
2023-04-16 16:29:22 +0200

    feat: add mine-sweeper solution

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const newMatrix = [];
+
+  for (let i = 0; i < rows; i++) {
+    newMatrix[i] = [];
+
+    for (let j = 0; j < cols; j++) {
+      let count = 0;
+      // ищем количество мин в соседних ячейках
+      // все возможные смещения по вертикали и диагонали от текущей ячейки (x = -1, 0, 1)
+      for (let x = -1; x <= 1; x++) {
+        // все возможные смещения по горизонтали и диагонали от текущей ячейки (y = -1, 0, 1)
+        for (let y = -1; y <= 1; y++) {
+          // в пределах ли матрицы ячейки вокруг + исключаем посчёт мины в текущей ячейке
+          if (i + x >= 0 && i + x < rows && j + y >= 0 && j + y < cols && (x !== 0 || y !== 0)) {
+            if (matrix[i + x][j + y] === true) {
+              count += 1;
+            } else {
+              count += 0;
+            }
+          }
+        }
+      }
+      newMatrix[i][j] = count;
+    }
+  }
+  return newMatrix;
 }
 
 module.exports = {
